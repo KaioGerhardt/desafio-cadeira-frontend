7d1fae7753c2155c039b9fdd6ab19fafa0095635
Kaio Gerhardt <gerhardt.kaio@gmail.com>
2024-04-06 16:26:26 -0300

    backend

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -6,6 +6,24 @@ class StudentModel {
         try{
             const students = await db.get(
                 `SELECT *
+                FROM student`, []
+            );
+            
+            if(students != null || students != undefined) {
+                return {code: 201, data: students};
+            }else{
+                return {code: 201, data: null, message: "Nenhum aluno encontrado."};
+            }
+
+        }catch(error){
+            return {code: 500, message: "error", errorMessage: error};
+        }
+    }
+
+    async getWithClass(body) {
+        try{
+            const students = await db.get(
+                `SELECT *
                 FROM student
                 LEFT JOIN enrollment 
                     ON enrollment.FK_idStudent = student.idStudent
@@ -23,6 +41,7 @@ class StudentModel {
             return {code: 500, message: "error", errorMessage: error};
         }
     }
+
 }
 
 module.exports = StudentModel;
